ef49b09b3b84c745b9c2a731923ebc3db1b214f1
sulavpanthi <panthisulav4@gmail.com>
2020-07-20 07:56:36 +0545

    [Added pagination]

--- django_assignment1/BlogProject/blogs/views.py
+++ django_assignment1/BlogProject/blogs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 from .models import Blog, Author
 from .forms import BlogCreateModelForm, AuthorCreateModelForm
 
@@ -7,7 +10,23 @@ from .forms import BlogCreateModelForm, AuthorCreateModelForm
 def blog_view(request):
 
     blogs = Blog.objects.all()
-    return render(request, 'blogs/blog.html', {'blogs':blogs})
+    
+    # pagination implementation start
+
+    paginator = Paginator(blogs, 5)
+    page = request.GET.get('page', 1)
+    try:
+        blogs_list = paginator.get_page(page)
+    except PageNotAnInteger:
+        blogs_list = paginator.get_page(1)
+    except EmptyPage:
+        blogs_list = paginator.get_page(paginator.num_pages)
+
+    # pagination implementation end
+
+    # remaining pagination implementation in blogs/blog.html
+
+    return render(request, 'blogs/blog.html', {'blogs':blogs_list})
 
 def landing_view(request):
     return render(request, 'blogs/landing.html')
